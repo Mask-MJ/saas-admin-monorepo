65cb5dea2699f7d4a93d10ab7009b4675bb3944d
mask <617473294@qq.com>
2024-12-13 17:32:07 +0800

    feat: init

---
+++ packages/@core/ui-kit/tabs-ui/postcss.config.mjs
@@ -0,0 +1,1 @@
+export { default } from '@saas/tailwind-config/postcss';
---
+++ packages/preferences/src/index.ts
@@ -0,0 +1,17 @@
+import type { Preferences } from '@saas-core/preferences';
+import type { DeepPartial } from '@saas-core/typings';
+
+/**
+ * 如果你想所有的app都使用相同的默认偏好设置，你可以在这里定义
+ * 而不是去修改 @saas-core/preferences 中的默认偏好设置
+ * @param preferences
+ * @returns
+ */
+
+function defineOverridesPreferences(preferences: DeepPartial<Preferences>) {
+  return preferences;
+}
+
+export { defineOverridesPreferences };
+
+export * from '@saas-core/preferences';
